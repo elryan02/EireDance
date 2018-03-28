588bf67092479f24c2a490e337447de64b6c288e
Emma <lkyemmaryan@gmail.com>
2018-03-28 17:51:42 -0400

    Connected flat data. Fixed variable inconsistency.

---
+++ src/routes/tExercises.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const { data } = require('C:/Users/Emma/desktop/clproject/data/exerciseData.json');
+const { tExercises } = data;
+
+router.get('/', function(req, res) {
+  res.render('tExercise', {
+    name: tExercises[0].name,
+    duration:tExercises[0].duration,
+    isDone: tExercises[0].duration
+  });
+ });
+
+
+module.exports = router;
